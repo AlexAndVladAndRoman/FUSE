fd7caf5144c220098a472864b3dc18297112d43f
SuperStrongDinosaur <alex_shirv@mail.ru>
2017-05-06 15:14:18 +0300

    add mp3 class

---
+++ src/mp3_file.c
@@ -0,0 +1,163 @@
+//
+//  mp3_file.c
+//  FUSEFilesystem
+//
+//  Created by Alex Shirvinsky on 06.05.17.
+//  Copyright © 2017 Alex Shirvinsky. All rights reserved.
+//
+
+#include "mp3_file.h"
+#include <string>
+#include <fstream>
+
+mp3_file::mp3_file(std::string path) {
+    TAGdata tagStruct;
+    std::ifstream mp3File;
+    char buffer[mp3TagSize + 1];
+    mp3File.open(path);
+    buffer[mp3TagSize] = '\0';
+    if (!mp3File) {
+        std::cerr << "Can't open " << path << std::endl;
+        return;
+    }
+    mp3File.seekg(-(mp3TagSize), std::ios::end);
+    if (mp3File.fail()) {
+        std::cerr << "Can't seek" << std::endl;
+        return;
+    }
+    mp3File.read(reinterpret_cast <char *>(&tagStruct), mp3TagSize);
+    if (mp3File.fail()) {
+        std::cerr << "Can't read" << std::endl;
+        return;
+    }
+    
+    mp3File.close();
+    artist = tagStruct.artist;
+    genre = tagStruct.genre;
+    album = tagStruct.album;
+    year = tagStruct.year;
+    tag = tagStruct.tag;
+    comment = tagStruct.comment;
+    title = tagStruct.title;
+}
+
+std::string mp3_file::get_artist() {
+    return artist;
+}
+
+std::string mp3_file::get_album() {
+    return album;
+}
+
+std::string mp3_file::get_year() {
+    return year;
+}
+
+std::string mp3_file::get_tag() {
+    return tag;
+}
+
+std::string mp3_file::get_comment() {
+    return comment;
+}
+
+std::string mp3_file::get_title() {
+    return title;
+}
+
+std::string mp3_file::get_genre() {
+    switch (genre)
+    {
+        case 0:
+            return "Blues";
+            break;
+        case 1:
+            return "Classic Rock";
+            break;
+            //TODO add more genres
+            /*  0.Blues
+             1.Classic Rock
+             2.Country
+             3.Dance
+             4.Disco
+             5.Funk
+             6.Grunge
+             7.Hip - Hop
+             8.Jazz
+             9.Metal
+             10.New Age
+             11.Oldies
+             12.Other
+             13.Pop
+             14.R&B
+             15.Rap
+             16.Reggae
+             17.Rock
+             18.Techno
+             19.Industrial
+             20.Alternative
+             21.Ska
+             22.Death Metal
+             23.Pranks
+             24.Soundtrack
+             25.Euro - Techno
+             26.Ambient
+             27.Trip - Hop
+             28.Vocal
+             29.Jazz + Funk
+             30.Fusion
+             31.Trance
+             32.Classical
+             33.Instrumental
+             34.Acid
+             35.House
+             36.Game
+             37.Sound Clip
+             38.Gospel
+             39.Noise
+             40.AlternRock
+             41.Bass
+             42.Soul
+             43.Punk
+             44.Space
+             45.Meditative
+             46.Instrumental Pop
+             47.Instrumental Rock
+             48.Ethnic
+             49.Gothic
+             50.Darkwave
+             51.Techno - Industrial
+             52.Electronic
+             53.Pop - Folk
+             54.Eurodance
+             55.Dream
+             56.Southern Rock
+             57.Comedy
+             58.Cult
+             59.Gangsta
+             60.Top 40
+             61.Christian Rap
+             62.Pop / Funk
+             63.Jungle
+             64.Native American
+             65.Cabaret
+             66.New Wave
+             67.Psychadelic
+             68.Rave
+             69.Showtunes
+             70.Trailer
+             71.Lo - Fi
+             72.Tribal
+             73.Acid Punk
+             74.Acid Jazz
+             75.Polka
+             76.Retro
+             77.Musical
+             78.Rock & Roll
+             79.Hard Rock*/
+        default:
+            return "Ethnic";
+            break;
+    }
+}
+
---
+++ src/mp3_file.h
@@ -0,0 +1,34 @@
+#ifndef mp3_file_h
+#define mp3_file_h
+#include <string>
+
+const int mp3TagSize = 128;
+
+class mp3_file {
+private:
+    struct TAGdata {
+        char tag[3];
+        char title[30];
+        char artist[30];
+        char album[30];
+        char year[4];
+        char comment[30];
+        int genre;
+    };
+    
+    std::string artist, album, year, tag, comment, title;
+    int genre;
+    
+public:
+    mp3_file(std::string path);
+    
+    std::string get_artist();
+    std::string get_album();
+    std::string get_year();
+    std::string get_tag();
+    std::string get_comment();
+    std::string get_title();
+    std::string get_genre();
+};
+
+#endif /* mp3_file_h */
